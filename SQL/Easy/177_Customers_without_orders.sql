28376b0a2baa47dbb66686d6b5051f7aaac9b304
Alex Plugin <plugin0785@gmail.com>
2026-03-06 17:55:44 +0100

    Added comments Task 177

--- SQL/Easy/177_Customers_without_orders.sql
+++ SQL/Easy/177_Customers_without_orders.sql
@@ -1,6 +1,19 @@
+-- Topic: SQL JOIN
+-- Difficulty: Easy
 -- LeetCode 177: Customers Who Never Order
-SELECT c.name AS Customers
+-- https://leetcode.com/problems/customers-who-never-order/
+
+-- Task:
+-- Write a solution to find all customers who never order anything.
+-- Return the result table in any order.
+
+SELECT c.name
 FROM Customers c
 LEFT JOIN Orders o
-ON c.id = o.customerId
-WHERE o.id IS NULL;
+on c.id = o.customerId
+WHERE o.id IS NULL
+
+-- Explanation:
+-- LEFT JOIN returns all rows from the Customers table.
+-- If a customer has no orders, the columns from Orders become NULL.
+-- Therefore, filtering with WHERE o.id IS NULL returns customers who never placed an order.
